c4f114eb71d0700af2cec45e16dd46e3f1724e62
KentaYanase <yana.ken.physics@gmail.com>
2019-01-25 22:05:03 +0900

    first commit

---
+++ Scripts/Script/Singleton/ManagerBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ICKX {
+
+	public abstract class ManagerBase<T> : MonoBehaviour where T : ManagerBase<T> {
+
+		private static T s_instance = null;
+		public static T instance {
+			get {
+				if (s_instance == null) {
+					s_instance = new GameObject (typeof (T).Name).AddComponent<T> ();
+					DontDestroyOnLoad (s_instance.gameObject);
+					if (!s_instance.isInitialized) {
+						s_instance.Initialize ();
+					}
+				}
+				return s_instance;
+			}
+		}
+
+		protected bool isInitialized = false;
+
+		public virtual void Initialize () {
+			isInitialized = true;
+		}
+	}
+}
---
+++ Scripts/Script/Singleton/SingletonBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ICKX {
+	public abstract class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T> {
+
+		private static T s_instance = null;
+		public static T Instance {
+			get {
+				if (s_instance == null) {
+					s_instance = FindObjectOfType<T> ();
+					if (!s_instance.isInitialized) {
+						s_instance.Initialize ();
+					}
+				}
+				return s_instance;
+			}
+		}
+
+		protected virtual bool isDontDestroyOnLoad { get { return false; } }
+
+		protected bool isInitialized = false;
+
+		protected virtual void Initialize () {
+			isInitialized = true;
+		}
+
+		protected void Awake () {
+            if(s_instance == null) {
+                s_instance = this as T;
+            }else {
+                if(s_instance != this) {
+                    Destroy (this);
+                }
+            }
+
+			if (s_instance.isDontDestroyOnLoad) {
+				DontDestroyOnLoad (s_instance.gameObject);
+			}
+			if (!s_instance.isInitialized) {
+				Initialize ();
+			}
+		}
+	}
+}
